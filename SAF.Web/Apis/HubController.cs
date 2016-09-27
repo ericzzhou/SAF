a1f9981df7e48065df22465c1665f6a573c35e29
eric.z.zhou <eric.z.zhou@outlook.com>
2016-09-28 00:06:08 +0800

    api framework

---
+++ SAF.Web/Apis/HubController.cs
@@ -0,0 +1,25 @@
+﻿using SAF.Web.Data.Model;
+using System.Web.Http;
+
+namespace SAF.Web.Apis
+{
+    [RoutePrefix("api/hub")]
+    public class HubController : ApiController
+    {
+        [Route("get/{id}/{age}")]
+        public string Get(int id, int age)
+        {
+            return "hub value " + id + " - " + age;
+        }
+
+        public Eric Get()
+        {
+            return new Eric()
+            {
+                FirstName = "Eric",
+                LastName = "Zhou",
+                Age = 28
+            };
+        }
+    }
+}
--- SAF.Web/App_Start/WebApiConfig.cs
+++ SAF.Web/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
 
 namespace SAF.Web
 {
@@ -14,11 +10,19 @@ namespace SAF.Web
         {
             // Web API 配置和服务
             // 将 Web API 配置为仅使用不记名令牌身份验证。
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
+
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            //默认返回 json  
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("datatype", "json", "application/json"));
+            //返回格式选择 datatype 可以替换为任何参数   
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("datatype", "xml", "application/xml"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
