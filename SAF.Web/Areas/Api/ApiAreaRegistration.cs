f60b2defbe496d6f12063e7f0410cd4f3cb1a5b7
eric.z.zhou <eric.z.zhou@outlook.com>
2016-09-27 22:03:43 +0800

    init

---
+++ SAF.Web/Areas/Api/ApiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SAF.Web.Areas.Api
+{
+    public class ApiAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Api";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Api_default",
+                "Api/{controller}/{action}/{id}",
+                new {controller="Default", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SAF.Web/Areas/Api/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAF.Web.Areas.Api.Models
+{
+    public class Users
+    {
+        public int Age { get; internal set; }
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
